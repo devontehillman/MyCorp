4c2bb9e169bf5bf4a1d9d2b57c8ffba244f5f784
devontehillman <hillmand@mail.gvsu.edu>
2021-03-01 13:45:32 -0500

    Add files via upload
    Initial files and starter code

---
+++ employee.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS employeesdb;
+
+USE employeesdb 
+
+CREATE TABLE department{
+id INT AUTO_INCREMENT NOT NULL,
+name VARCHAR(30),
+PRIMARY KEY (id)
+}
+
+CREATE TABLE roles{
+id INT AUTO_INCREMENT NOT NULL,
+title VARCHAR(30),
+salary DECIMAL, 
+department_id INT,
+PRIMARY KEY (id)
+}
+
+CREATE TABLE employees{
+id INT AUTO_INCREMENT NOT NULL,
+firstname VARCHAR(),
+lastname VARCHAR(),
+PRIMARY KEY (id)
+};
---
+++ server.js
@@ -0,0 +1,114 @@
+// const express = require('express');
+ const inquirer = require('inquirer');
+// // install console table
+
+// app = express();
+// event handler 
+
+// app.listen()
+
+// Add departments, roles, employees View departments, roles, employees Update employee roles
+inquirer.prompt([
+    {
+        type: 'list',
+        name:'userChoice',
+        choices:['View departments','View roles','View employees','Add departments','Add roles','Add employees', 'Update employees role']
+    }
+]).then((data)=>{
+    
+    switch(data.userChoice){
+        case 'View departments':
+        
+        break;
+
+        case'View roles':
+        
+        break;
+
+        case'View employees':
+        
+        break;
+
+        case'Add departments':
+        //prompt name
+        departmentPrompt();
+        
+        break;
+
+        case'Add roles':
+        //title, salary, department id 
+        rolePrompt();
+        
+        break;
+
+        case'Add employees':
+        // add first, last, department?
+        employeePrompt();
+        
+        break;
+
+        case'Update employees role':
+        
+        break;
+    }
+})
+
+
+
+//Department
+function departmentPrompt(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name:'depName',
+            message:'Department name'
+        }
+    ]).then((data)=>{}); 
+};
+
+//Role
+function rolePrompt(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name:'jobTitle',
+            message:'Job title'
+        },
+        {
+            type: 'input',
+            name:'salary',
+            message:'Salary'
+        },
+        {
+            type: 'input',
+            name:'depID',
+            message:'Department ID'
+        }
+    ]).then((data)=>{}); 
+};
+
+//Employee
+function employeePrompt(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name:'firstName',
+            message:'First name'
+        },
+        {
+            type: 'input',
+            name:'lastName',
+            message:'Last name'
+        }
+    ]).then((data)=>{}); 
+}  
+
+// view any thing 
+// access table from db and display 
+
+// add departments,role, employees
+// access database and insert into it
+
+//update employees is this a modifier?
+// delete a employee
+//add an employee
