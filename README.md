14645edda8c326bf7e65c8e5799e1be9fb1600c5
Devonte Hillman <hillmand@mail.gvsu.edu>
2021-03-03 14:16:58 -0500

    added functionality for exit through main menu, added comments in code, updated  UI

---
+++ README.md
@@ -0,0 +1,54 @@
+# Noted
+
+# Description
+This is a command line application that is designed for a Management company.This is program is a CMS(content management system) that is designed to allow a manager to view, add, and edit their company's employees, departments, and roles. This command line application incorporates the use of a database to store the company's information.  [Deployed Webpage](https://obscure-inlet-06343.herokuapp.com/)
+
+# Table of Contents
+1. [Description](#Description)
+2. [Installation](#Installation)
+3. [Usage](#Usage)
+4. [licence](#License)
+5. [Technologies](#Technologies)
+6. [Future Improvements](#Future-Improvements)
+
+## Finished-Page
+![My web page](./Assets/homepage.png)
+![My web page](./Assets/noteswp.png)
+
+
+# Installation
+Go to this https://github.com/devontehillman/MyCorp and the clone repository. To run this application on your local machine, open integrated terminal at the MyCorp file type 'npm install' to install the included modules. If you want to use the seed file you can copy paste it into the mysql work bench and execute it. Another option is to upload the sql file through the terminal by running 'mysql -u root -p'. Enter your password for mysql. Then, in the terminal run 'source employee.sql'. Check inside the mysql work bench to see if the database was created. Now, before running the server file you have to establish a connection to the database. Create a .gitignore file and in it type config (this is so that your password is not exposed if you choose to do a git push).Now in the config folder in the local.js file enter your mysql password into the password property and save.
+
+# Usage
+ In the terminal type 'node server'. This should start the program and prompt you for the action you want to execute. Scroll through the dropdown menu and press enter on the action you want to execute.
+
+# license
+To see information on the no license click the badge at the top of this README.
+
+# Technologies
+* Nodejs
+    * mysql
+    * console table 
+    * inquirer
+* SQL
+    * MySQL Workbench
+
+# Included features
+  * View departments, roles, employees
+  
+  * Add departments, roles, employees
+
+  * Update employee roles by employee ID
+
+# Future-Improvements
+Adding these additional functionalities: 
+* Update employee managers
+
+* View employees by manager
+
+* Delete departments, roles, and employees
+
+* View the total utilized budget of a department -- ie the combined salaries of all employees in that department
+
+* Integration into a website for a better UX.
+
--- server.js
+++ server.js
@@ -1,89 +1,58 @@
-const express = require('express');
-const mysql = require('mysql');
 const config = require('./config/local')
-const router = express.Router()
 const cTable = require('console.table');
 const inquirer = require('inquirer')
-
+const mysql = require('mysql');
+const chalk = require('chalk');
 
 // Add departments, roles, employees View departments, roles, employees Update employee roles
-inquirer.prompt([
-    {
-        type: 'list',
-        name:'userChoice',
-        choices:['View departments','View roles','View employees','Add departments','Add roles','Add employees', 'Update employees role']
-    }
-]).then((data)=>{
-    
-    switch(data.userChoice){
-        case 'View departments':
-            connection.connect((err) => {
-                if (err) throw err;
-                console.log(`connected as id ${connection.threadId}`);
-                readTable(`department`)
-            });
-        break;
-
-        case'View roles':
-            connection.connect((err) => {
-                if (err) throw err;
-                console.log(`connected as id ${connection.threadId}`);
-                readTable(`roles`)
-            });
-        break;
-
-        case'View employees':
-            connection.connect((err) => {
-                if (err) throw err;
-                console.log(`connected as id ${connection.threadId}`);
-                readTableEmployee()
-            });
-        break;
-
-        case'Add departments':
-        //prompt name
-        departmentPrompt();
-        
-        break;
-
-        case'Add roles':
-        //title, salary, department id 
-        rolePrompt();
-        
-        break;
-
-        case'Add employees':
-        // add first, last, department?
-        employeePrompt();
-        
-        break;
-
-        case'Update employees role':
-        updateTablePromts()
-        break;
-    }
-})
-//update table 
-function updateTablePromts(){
+function mainPrompt(){
     inquirer.prompt([
         {
-            type: 'input',
-            name:'employeeID',
-            message:'Employee\'s ID that you want to update:'
-        },
-        {
-            type: 'input',
-            name:'newRoleId',
-            message:'Employee\'s New Role ID:'
+            type: 'list',
+            name:'userChoice',
+            message: 'Choose an action:',
+            choices:['View departments','View roles','View employees','Add departments','Add roles','Add employees','Update employees role','Exit']
         }
     ]).then((data)=>{
-        updateTable(data.employeeID,data.newRoleId)
-        //this info will connect to a connection query to update the db
-    }); 
-};
+        switch(data.userChoice){
+            case 'View departments':
+                readTable(`department`)
+            break;
 
+            case'View roles':
+                readTable(`roles`)
+            break;
 
-//Department
+            case'View employees':
+                readTableEmployee()
+            break;
+
+            case'Add departments':
+                departmentPrompt();
+            break;
+
+            case'Add roles':
+                rolePrompt();
+            break;
+
+            case'Add employees':
+                employeePrompt();
+            break;
+
+            case'Update employees role':
+                updateTablePrompts()
+            break;
+
+            case'Exit':
+            connection.end()
+            console.log('Untill Next time!')
+            break;
+
+        }
+    })
+}
+
+//Department Prompts
 function departmentPrompt(){
     inquirer.prompt([
         {
@@ -92,12 +61,12 @@ function departmentPrompt(){
             message:'Department name'
         }
     ]).then((data)=>{
-        createEmployee(`department`,{name: data.depName});
-        //this info will connect to a connection query to update the db
+        //Function that takes user input and updates the DB
+        createTableEl(`department`,{name: data.depName});
     }); 
 };
 
-//Role
+//Role Prompts
 function rolePrompt(){
     inquirer.prompt([
         {
@@ -116,12 +85,12 @@ function rolePrompt(){
             message:'Department ID'
         }
     ]).then((data)=>{
-        createEmployee(`roles`,{title: data.jobTitle, salary: data.salary, department_id: data.depID});
-        //readTable(`roles`)
+        //Function that takes user input and updates the DB
+        createTableEl(`roles`,{title: data.jobTitle, salary: data.salary, department_id: data.depID});
     }); 
 };
 
-//Employee
+//Employee Prompts 
 function employeePrompt(){
     inquirer.prompt([
         {
@@ -133,32 +102,43 @@ function employeePrompt(){
             type: 'input',
             name:'lastName',
             message:'Last name'
+        },
+        {
+            type: 'input',
+            name:'roleID',
+            message:'Role ID'
+        },
+        {
+            type: 'input',
+            name:'managerID',
+            message:'Managers ID'
         }
     ]).then((data)=>{
-        createEmployee(`employees`, {firstname: data.firstName, lastname: data.lastName});
-        //readTable(`employees`)
+        //Function that takes user input and updates the DB
+        createTableEl(`employees`, {firstname: data.firstName, lastname: data.lastName, role_id: data.roleID, manager_id: data.managerID});
     }); 
 }  
 
 // view any thing 
-// access table from db and display 
+// view employee has its own function because of its special query found at the bottom of this page
 const readTableEmployee = (tableName) => {
-    
+    console.log(`\nViewing the Employee table...\n`)
     connection.query(employeeQuery, (err, res) => {
       if (err) throw err;
       // Log all results of the SELECT statement
       console.table(res);
-      connection.end();
+      restartPrompt()
     });
   }
 // access table from db and display 
 const readTable = (tableName) => {
-    console.log(`Viewing the ${tableName} table...\n`);
+    console.log(`\nViewing the ${tableName} table...\n`);
     connection.query(`SELECT * FROM ` + tableName, (err, res) => {
       if (err) throw err;
       // Log all results of the SELECT statement
       console.table(res);
-      connection.end();
+      restartPrompt()
+      
     });
   };
 // add departments,role, employees
@@ -168,12 +148,30 @@ const createTableEl = (selection,queryObj) => {
       'INSERT INTO '+ selection +' SET ?',queryObj,
       (err, res) => {
         if (err) throw err;
-        console.table(`${res.affectedRows} product inserted!\n`);
-        // Call updateProduct AFTER the INSERT completes
-        readTable(selection);
+        console.table(`Added new ` + selection);
+        restartPrompt()
       }
     );
 }
+
+//Table Prompts
+function updateTablePrompts(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name:'employeeID',
+            message:'Employee\'s ID that you want to update:'
+        },
+        {
+            type: 'input',
+            name:'newRoleId',
+            message:'Employee\'s New Role ID:'
+        }
+    ]).then((data)=>{
+        //Function that takes user input and updates the DB
+        updateTable(data.employeeID,data.newRoleId)
+    }); 
+};
 
 //update employees is this a modifier?
 const updateTable = (employee_id, newRoleId) => {
@@ -181,23 +179,65 @@ const updateTable = (employee_id, newRoleId) => {
     const query = connection.query(
       'UPDATE employees SET ? WHERE ?',[{role_id:newRoleId},{id:employee_id},], (err, res) => {
         if (err) throw err;
-        console.log(`${res.affectedRows} products updated!\n`);
-        // Call deleteProduct AFTER the UPDATE completes
-        connection.end();
+        console.log(`Employee role updated!\n`);
+        restartPrompt()
       }
     );
     //add console log "employee with id updated role to..."
   };
 
-// delete a Employee
+// restart the prompt function
+function restartPrompt(){
+    inquirer.prompt([
+        {
+            type: 'confirm',
+            name:'userConfim',
+            message: 'would you like to do something else?',
+        }
+    ]).then((data)=>{
+        if(data.userConfim){
+            mainPrompt()
+        }else{
+            connection.end()
+            console.log('Untill Next time!')
+        }
+    })
+    };
+// delete an Employee
 //add an employee
 
 // create mysql connection
 const connection = mysql.createConnection(config.db);
 
-// create routes
+// Init program
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+    console.log(chalk.blue(chalkText));
+    mainPrompt()
+});
+
+
+// query string for viewing employees with additional information
 const employeeQuery= 
-`SELECT  employees.firstname, employees.lastname, roles.title, department.name dePartment, roles.salary
+`SELECT employees.id, employees.firstname, employees.lastname, roles.title, department.name dePartment, roles.salary
 FROM employees
 INNER JOIN department ON employees.role_id=department.id
 INNER JOIN roles ON employees.role_id = roles.department_id`
+
+const chalkText = 
+`------------------------------------------------------------------------------------------------------ 
+ __       ___    ____      ____     _________         ____________       _________         ___________
+/  \\    /   |  /   /     /   /    /           \\      /           / \\    |         /\\      /          /\\
+$$$ \\  /$$$ |  $$$$      $$$$ |  /$$$$$$$$$$$  \\    /$$$$$$$$$$$$   \\   $$$$$$$$$$  \\    $$$$$$$$$$$$  \\
+$$$$ \\/$$$$ |   $$$$    $$$$ |  $$$$$$$$$$$$$$$|   /$$$$$$$$$$$$$$$  |  $$$$$$$$$$$  \\   $$$      $$$$  | 
+$$$$$$$$$$  |    $$$$  $$$$ |   $$$   ______$$$|   $$  ________   $$ |  $$$       $$  |  $$$       $$$$ |
+$$$ $$ $$$  |       $$$$  |     $$$  |             $$ |        |  $$ |  $$$       $$  |  $$$       $$$$ |
+$$$    $$$  |       $$$$ |      $$$  |       ____  $$ |        |  $$ |  $$$$$$$$$$$  /   $$$$$$$$$$$$  /
+$$$    $$$  |       $$$$ |      $$$  |______/   /| $$ |________|  $$ |  $$$   $$$$  /    $$$  ________/    
+$$$    $$$  |       $$$$ |      $$$          $$$ | $$             $$ |  $$$    $$$$ \\    $$$ |           
+$$$    $$$  |       $$$$ |       $$$$$$$$$$$$$$ /   $$$$$$$$$$$$$$$ /   $$$     $$$$ \\   $$$ |            
+$$$    $$$_/        $$$$_|        $$$$$$$$$$$$_/     $$$$$$$$$$$$$_/    $$$      $$$$_\\  $$$_|           
+
+-------------------------------------------------------------------------------------------------------
+`;
